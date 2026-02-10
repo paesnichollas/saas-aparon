35d7037795433be9fd7a2f627379b91e41349a5a
Nichollas Paes <paesnichollas@gmail.com>
2026-02-09 23:12:58 -0300

    fix(footer): update copyright year to 2026

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-muted p-7">
       <p className="text-foreground text-xs font-semibold">
-        &copy; 2025 Aparatus
+        &copy; 2026 Aparatus
       </p>
       <p className="text-muted-foreground text-xs">
         Todos os direitos reservados.
